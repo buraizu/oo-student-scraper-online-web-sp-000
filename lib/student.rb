4eb5808886053699b544aaf77b3fca2f84a85e2d
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-20 08:13:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,11 +18,16 @@ class Student
     end
   end
 
-  def add_student_attributes(student_hash)
-    
+  def add_student_attributes(attributes_hash)
+    @twitter = attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin]
+    @github = attributes_hash[:github]
+    @blog = attributes_hash[:blog]
+    @profile_quote = attributes_hash[:profile_quote]
+    @bio = attributes_hash[:bio]
   end
 
-  
+
 
 end
 
